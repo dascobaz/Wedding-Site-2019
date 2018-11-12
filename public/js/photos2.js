d9b3273c6f3f41e240c87a03707dcaf5392e2110
jahnava <jahnavalmt@gmail.com>
2018-11-12 11:07:17 -0500

    created new photos page

---
+++ public/js/photos2.js
@@ -0,0 +1,54 @@
+function openModal(idx) {
+  var isModalOpen = $("body").hasClass("modal-open");
+
+  if (!isModalOpen) {
+    document.getElementById("sliderModal").style.height = "100%"
+        $("body").addClass("modal-open");
+  }
+
+  var $carousel = $(".gallery").flickity("resize");
+
+  if (parseInt(idx)) {
+    $carousel = $(".js-flickity").flickity("select", parseInt(idx) - 1);
+  }
+
+  var $caption = $('.caption');
+  // Flickity instance
+  var flkty = $carousel.data('flickity');
+
+  $carousel.on( 'select.flickity', function() {
+    // set image caption using img's alt
+    $caption.text( flkty.selectedElement.children.item(0).alt );
+  });
+}
+
+function closeModal() {
+  document.getElementById("sliderModal").style.height = "0%";
+  $("body").removeClass("modal-open");
+}
+
+function openGallery() {
+  if (!$("body").hasClass("modal-open")) {
+    document.getElementById("galleryModal").style.height = "100%";
+
+    // for (var i = 0; i < children.length; i++){
+    // console.log(children[i]);
+    // }
+    [].forEach.call(document.querySelectorAll("img[data-src]"), function(img) {
+      img.setAttribute("src", img.getAttribute("data-src"));
+      img.onload = function() {
+        img.removeAttribute("data-src");
+      };
+    });
+    $("body").addClass("modal-open");
+  }
+}
+
+function closeGallery(idx) {
+  document.getElementById("galleryModal").style.height = "0%";
+  $("body").removeClass("modal-open");
+
+  if (parseInt(idx)) {
+    openModal(idx);
+  }
+}
